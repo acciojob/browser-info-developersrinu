c35710116bdae4ca29bb7973f4d3bd7237863033
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-07-02 18:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+    const browserName = navigator.userAgent;
+    const version = parseFloat(navigator.appVersion);
+    const message = "You are using " + browserName + " version " + version;
+    document.getElementById("browser-info").textContent = message;
